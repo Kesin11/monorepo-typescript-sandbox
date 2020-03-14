816ca7223aac6e3195b17d377cdd43350ea87940
Kenta Kase <kesin1202000@gmail.com>
2020-03-14 21:16:45 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+      "standard",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "parserOptions": {
+      "sourceType": "module",
+  },
+}
