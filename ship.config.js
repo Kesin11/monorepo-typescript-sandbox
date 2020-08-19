94885aa096b123d037921d5af5e55cfe27aedbf3
Kenta Kase <kesin1202000@gmail.com>
2020-08-19 21:27:43 +0900

    chore: Add automate release flow with github actions

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Don't pubilsh to npm
+  publishCommand: ({ _isYarn, _tag, _defaultCommand, _dir }) => 'true',
   monorepo: {
     mainVersionFile: 'lerna.json',
     packagesToBump: ['packages/*'],
