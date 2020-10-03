95d4d365ef116e03910079b1293a5ed06ca8992c
Kenta Kase <kesin1202000@gmail.com>
2020-10-03 23:17:36 +0900

    refactor: fix eslint rule 'no-use-before-define'

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -1,7 +1,7 @@
+export type Resource = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 export interface IProvider {
   generate: () => Resource[];
 }
 export interface IConsumer {
   consume: (resources: Resource[]) => Resource | undefined;
 }
-export type Resource = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
