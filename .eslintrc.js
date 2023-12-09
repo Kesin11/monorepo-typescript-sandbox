6cec93850669cb6c31073eeec91434ac48388c76
Kenta Kase <kesin1202000@gmail.com>
2023-12-09 20:56:44 +0900

    Reinitialize eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
-  "extends": [
-      "standard",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-  ],
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-      "@typescript-eslint"
-  ],
-  "parserOptions": {
-      "sourceType": "module",
-  },
-  "rules": {
-    "@typescript-eslint/interface-name-prefix": "off"
-  }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
